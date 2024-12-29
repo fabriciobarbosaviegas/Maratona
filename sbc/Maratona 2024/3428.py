859f37673028cf43a2afd81c598ba078fa33119b
Fabrício Barbosa Viegas <incy8498@gmail.com>
2024-12-29 15:30:41 -0300

    3428 e 3432

---
+++ sbc/Maratona 2024/3428.py
@@ -0,0 +1,15 @@
+n = int(input())
+b = list(map(int, input().split()))
+
+flechas = {}
+c = 0
+
+for altura in b:
+    if flechas.get(altura, 0) > 0:
+        flechas[altura] -= 1
+        flechas[altura - 1] = flechas.get(altura - 1, 0) + 1
+    else:
+        c += 1 
+        flechas[altura - 1] = flechas.get(altura - 1, 0) + 1
+
+print(c)
---
+++ sbc/Maratona 2024/3432.py
@@ -0,0 +1,2 @@
+n = [int(i) for i in input().split()]
+print('F' if 9 in n else 'S')
